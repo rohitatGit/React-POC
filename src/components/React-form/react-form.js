50b0208702f470846e7c98a1918c00db48cde9c1
Aman Jain <AmanJa@hcl.com>
2019-01-10 17:42:54 +0530

    committed by Aman for React Form (10/01/2018)

--- src/components/React-form/react-form.js
+++ src/components/React-form/react-form.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react';
+import {
+  FormBuilder,
+  FieldGroup,
+  FieldControl,
+  Validators
+} from "react-reactive-form";
+import { Col, Form, FormGroup, Label, Input } from 'reactstrap';
 
-const reactform = () => {
+class reactform extends Component {
+  reactform = FormBuilder.group({});
+
+  handleReset = e => {
+    this.reactform.reset();
+  };
+  handleSubmit = e => {
+    console.log("Form values", this.reactform.value);
+    e.preventDefault();
+  };
+  render() {
     return (
-      <div>
-        <p>reactform</p>
-      </div>
-    )
+      <FieldGroup
+        control={this.reactform}
+        render={({ get, invalid, reset, value }) => (
+          <Form onSubmit={this.handleSubmit}>
+            <label>Username:{" "}</label>
+            <FieldControl
+              name="username"
+              options={{ validators: Validators.required }}
+              render={({ handler, touched, hasError }) => (
+                <div>
+                  <Input placeholder="Please enter the User Name" {...handler()} />
+                  <span>
+                    {touched && hasError("required") && "Username is required"}
+                  </span>
+                </div>
+              )}
+            />
+            <br />
+            <label>Password:</label>
+          <FieldControl
+              name="password"
+              options={{ validators: Validators.required }}
+              render={({ handler, touched, hasError }) => (
+                <div>
+                  <input {...handler()} />
+                  <span>
+                    {touched && hasError("required") && "Password is required"}
+                  </span>
+                </div>
+              )}
+            />
+            <br />
+            <button type="button" onClick={this.handleReset}>
+              Reset
+          </button>
+            <button type="submit" disabled={invalid}>
+              Submit
+          </button>
+          </Form>
+        )}
+      />
+    );
   }
+}
 
-  export default reactform;
+export default reactform;
